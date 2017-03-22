51f6c85361e0c9f6baf5d872c95772895dd4002c
Shaun Warman <swarman@paypal.com>
2017-03-22 00:22:21 -0500

    Initial commit

---
+++ datacontainer/server.js
@@ -0,0 +1,18 @@
+console.log(process.cwd());
+const Express = require('express');
+
+const App = Express();
+
+const { APP_NAME, APP_PORT } = process.env;
+
+App.get('/', (req, res) => {
+  res.send('Hello there!');
+});
+
+App.get('/app', (req, res) => {
+  res.send(`App name ${APP_NAME}`);
+});
+
+App.listen(APP_PORT, () => {
+  console.log(`App started...`);
+});
